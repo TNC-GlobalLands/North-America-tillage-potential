1e35ea8a3968cecbe6ae06100f5ab0248bb5a558
TNC-Eugene <eyacobson@tnc.org>
2020-05-26 17:27:00 -0400

    Fetching Daymet precipitation data
    Fetch Daymet precip data for given years, extent, number of retries in case of failure, & wait time between retries

--- R/data_processing.R
+++ R/data_processing.R
@@ -70,3 +70,46 @@ fetch_topowx <- function(years, county) {
     }
   }
 }
+
+###Fetching Daymet precipitation dailies###
+#Gets Daymet precipitation dailies. Arguments:
+#1). Vector of years for which to get daily data
+#2). Polygon of county (feature should include "STATE_NAME" attribute with name of state)
+#3). Number of attempts, in case server fails to respond
+#4). Downtime between attempts (in seconds)
+fetch_daymet <- function(years, county, retrynum, sleepnum) {
+  labelstring <- paste0(gsub(" ", "", as.character(county$NAME)), "_Co_", gsub(" ", "", as.character(county$STATE_NAME))) #Filename for .tif written to disk
+  countyproj <- spTransform(county, crs("+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 "))
+  
+  get_data <- function() {
+    daymet <- get_daymet(template = countyproj,
+                         label = labelstring,
+                         elements = "prcp",
+                         years = years,
+                         region = "na",
+                         tempo = "day",
+                         extraction.dir = getwd(),
+                         progress = TRUE)
+    return(daymet)
+  }
+  
+  #Function that will attempt data download & return NULL if it fails
+  get_data_attempt <- purrr::possibly(get_data, otherwise = NULL)
+  
+  result <- NULL
+  try_number <- 1
+  
+  #Attempt data download for set number of times or until function returns a proper result
+  while(is.null(result) && try_number <= retrynum) {
+    print(paste0("Attempt: ", try_number))
+    try_number <- try_number + 1
+    result <- get_data_attempt()
+    Sys.sleep(sleepnum)
+  }
+  
+  if (try_number > retrynum) {
+    warning("Could not get data!")
+  }
+  
+  return(result)
+}
